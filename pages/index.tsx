fef33ce80d1ed720aed9d392564b4d15f90fae75
HyunSJ <dudtod1596@gmail.com>
2022-12-09 20:13:16 +0900

    add open graph content

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,10 +23,19 @@ export default function Home() {
     <div className="z-0 bg-gray-100 scroll-smooth">
       <Head>
         <link rel="icon" href="/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"></link>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"></link>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"></link>
-        <link rel="manifest" href="/favicon/site.webmanifest"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://hyunseungjai.vercel.app/" />
+        <meta property="og:title" content="현승재의 포트폴리오" />
+        <meta property="og:image" content="/hsj-portfolio-logo.webp" />
+        <meta property="og:description" content="웹 프론트엔드 포트폴리오 입니다" />
+        <meta property="og:site_name" content="현승재 | 웹 프론트엔드 개발자" />
+        <meta property="og:locale" content="kor" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <title>현승재 | 웹 프론트엔드 개발자</title>
         <meta name="description" content="프론트엔드 개발자 포트폴리오" />
       </Head>
